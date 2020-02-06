7603536dff28224d026d43f83c355cce54be5c7f
Tyler Eddy <tylereddy96@gmail.com>
2020-02-06 17:15:20 +0000

    added test

--- src/test/java/com/qa/test/OrderTest.java
+++ src/test/java/com/qa/test/OrderTest.java
@@ -19,7 +19,7 @@ public class OrderTest {
 		order.setDiscount(90);
 		assertEquals(1,order.getId());
 		assertEquals(6.39,order.getCost(),0.001);
-		assertEquals(1, order.getId());
+		assertEquals(4, order.getCustomerId());
 		assertEquals(90, order.getDiscount());
 
 	}

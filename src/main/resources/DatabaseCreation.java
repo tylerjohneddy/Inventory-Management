1cac8d801acb88bbed1778936e2ba6e973f6248a
Tyler Eddy <tylereddy96@gmail.com>
2020-02-06 09:59:42 +0000

    first logger properties

--- src/main/resources/DatabaseCreation.java
+++ src/main/resources/DatabaseCreation.java
@@ -2,8 +2,8 @@
 public class DatabaseCreation {
 	public void tableSQL() {
 		String inventoryManagementDatabase = "create database if not exists inventory_management;";
-		
-		//String useDatabase = "use inventory_management;";
+
+		// String useDatabase = "use inventory_management;";
 
 		String customersTable = "create table if not exists customers(id int primary key auto_increment,forename varchar(50) not null,surname varchar(50) not null);";
 
@@ -14,7 +14,6 @@ public class DatabaseCreation {
 		String itemOrderTable = "create table if not exists item_order(id int primary key auto_increment,order_id int,item_id int,item_quantity int not null,sold_cost float(7,2) default(0.00),foreign key (item_id) references items(id),foreign key (order_id) references orders(id));";
 
 		String usersTable = "create table if not exists users(id int primary key auto_increment,name varchar(40) not null unique,password varchar(20) not null);";
-
 
 	}
 }
